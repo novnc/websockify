cb72569d1e3b4f34d0f5dc966aa438f6937f2496
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2011-10-11 17:25:38 +0200

    Eggify websockify

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import glob
+import os
+
+version = '0.1-dev'
+name = 'websockify'
+long_description = open("README.md").read() + "\n" + \
+    open("CHANGES.txt").read() + "\n"
+
+setup(name=name,
+      version=version,
+      description="Websockify.",
+      long_description=long_description,
+      classifiers=[
+          "Programming Language :: Python",
+        ],
+      keywords='noVNC websockify',
+      license='LGPLv3',
+      url="https://github.com/kanaka/websockify",
+      author="Joel Martin",
+      author_email="github@martintribe.org",
+
+      packages="",
+      include_package_data=True,
+      install_requires=['numpy'],
+      zip_safe=False,
+      entry_points={
+        'console_scripts': [
+          'websockify = websockify:websockify_init',
+        ]
+      },
+    )
