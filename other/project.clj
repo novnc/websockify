0f1512c3cafc033652de2fd49eac7b751f851d07
Joel Martin <github@martintribe.org>
2012-01-26 15:31:21 -0600

    Clojure Websockify using Aleph - broken.

---
+++ other/project.clj
@@ -0,0 +1,14 @@
+(defproject websockify "1.0.0-SNAPSHOT"
+  :description "Websockify"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [aleph "0.2.0"]
+
+                 [ring/ring-jetty-adapter "1.0.0-beta2"]
+                 ;[ring-json-params "0.1.3"]
+                 [compojure "0.6.5"]
+                 ;[commons-codec/commons-codec "1.4"]
+                 ;[clj-base64 "0.0.0-SNAPSHOT"]
+
+                 ]
+;  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  )
