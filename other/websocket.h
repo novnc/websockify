e69d4a45464b77231944a9630a9004fdb9ef160e
Pierre Ossman <ossman@cendio.se>
2017-10-18 16:35:52 +0200

    Properly declare websocket functions in header

--- other/websocket.h
+++ other/websocket.h
@@ -65,6 +65,8 @@ typedef struct {
 } settings_t;
 
 
+int resolve_host(struct in_addr *sin_addr, const char *hostname);
+
 ssize_t ws_recv(ws_ctx_t *ctx, void *buf, size_t len);
 
 ssize_t ws_send(ws_ctx_t *ctx, const void *buf, size_t len);
@@ -82,3 +84,17 @@ ssize_t ws_send(ws_ctx_t *ctx, const void *buf, size_t len);
 #define handler_msg(...) gen_handler_msg(stdout, __VA_ARGS__);
 #define handler_emsg(...) gen_handler_msg(stderr, __VA_ARGS__);
 
+void traffic(const char * token);
+
+int encode_hixie(u_char const *src, size_t srclength,
+                 char *target, size_t targsize);
+int decode_hixie(char *src, size_t srclength,
+                 u_char *target, size_t targsize,
+                 unsigned int *opcode, unsigned int *left);
+int encode_hybi(u_char const *src, size_t srclength,
+                char *target, size_t targsize, unsigned int opcode);
+int decode_hybi(unsigned char *src, size_t srclength,
+                u_char *target, size_t targsize,
+                unsigned int *opcode, unsigned int *left);
+
+void start_server();
