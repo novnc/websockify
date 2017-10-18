89bbb64145b4c6f8784860ae451e6e1fafbd2c7d
Pierre Ossman <ossman@cendio.se>
2017-10-18 16:35:30 +0200

    Include string.h where needed

--- other/websocket.c
+++ other/websocket.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <strings.h>
+#include <string.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <sys/stat.h>
--- other/websockify.c
+++ other/websockify.c
@@ -11,6 +11,7 @@
 #include <errno.h>
 #include <limits.h>
 #include <getopt.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
