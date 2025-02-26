0f0217d0bf5427503f8bb055a8a832b52fc407ff
d0tiKs <alessandr.pisu@gmail.com>
2025-02-26 20:58:10 +0100

    Add a new auth class HtPasswdAuth, to dissociate the logic between HttpBasicAuth and HtPasswdAuth

--- websockify/auth_plugins.py
+++ websockify/auth_plugins.py
@@ -68,6 +68,51 @@ class BasicHTTPAuth():
     def validate_creds(self, username, password):
         if '%s:%s' % (username, password) == self.src:
             return True
+        else:    
+            return False
+
+    def auth_error(self):
+        raise AuthenticationError(response_code=403)
+
+    def demand_auth(self):
+        raise AuthenticationError(response_code=401,
+                                  response_headers={'WWW-Authenticate': 'Basic realm="Websockify"'})
+
+class HtPasswdAuth():
+    """Verifies Basic Auth headers against a htpasswd. Specify src as the path to the htpasswd file"""
+
+    def __init__(self, src=None):
+        self.src = src
+
+    def authenticate(self, headers, target_host, target_port):
+        import base64
+        auth_header = headers.get('Authorization')
+        if auth_header:
+            if not auth_header.startswith('Basic '):
+                self.auth_error()
+
+            try:
+                user_pass_raw = base64.b64decode(auth_header[6:])
+            except TypeError:
+                self.auth_error()
+
+            try:
+                # http://stackoverflow.com/questions/7242316/what-encoding-should-i-use-for-http-basic-authentication
+                user_pass_as_text = user_pass_raw.decode('ISO-8859-1')
+            except UnicodeDecodeError:
+                self.auth_error()
+
+            user_pass = user_pass_as_text.split(':', 1)
+            if len(user_pass) != 2:
+                self.auth_error()
+
+            if not self.validate_creds(*user_pass):
+                self.demand_auth()
+
+        else:
+            self.demand_auth()
+
+    def validate_creds(self, username, password):
         if not self.src:
             return False
         try:
